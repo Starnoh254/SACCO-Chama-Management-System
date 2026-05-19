f0d7035b48e5b4de18636947184a1f0134250f8e
Starnoh John <130779431+Starnoh254@users.noreply.github.com>
2026-05-19 19:31:51 +0300

    first commit

---
+++ src/main/java/com/starnoh/sacco_management/SaccoManagementApplication.java
@@ -0,0 +1,13 @@
+package com.starnoh.sacco_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaccoManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaccoManagementApplication.class, args);
+	}
+
+}
